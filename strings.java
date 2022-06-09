e233b353d4cf51415c3539c84a1d27e5170a1811
aayushi-jain-5 <101443827+aayushi-jain-5@users.noreply.github.com>
2022-06-09 23:18:25 +0530

    Update strings.java

--- strings.java
+++ strings.java
@@ -4,15 +4,15 @@ import java.util.*;
 public class String{
     public static void main(Strings[] args) {
         //concatenation
-        // String firstName="tony";
-        // String lastName="stark";
-        // String fullName = firstName +"@" + lastName;
-        // System.out.println(fullName.length());
+        String firstName="tony";
+        String lastName="stark";
+        String fullName = firstName +"@" + lastName;
+        System.out.println(fullName.length());
 
-        // //charAt
-        // for(int i=0;i< fullName.length();i++){
-        //     System.out.println(fullName.charAt(i));
-        // }
+         //charAt
+        for(int i=0;i< fullName.length();i++){
+            System.out.println(fullName.charAt(i));
+        }
 
         //compare
         String name1 ="Tony";
