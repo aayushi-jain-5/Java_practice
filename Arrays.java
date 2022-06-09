63fa031f82f2bbd295f9827c77840b615f8de4a3
aayushi-jain-5 <101443827+aayushi-jain-5@users.noreply.github.com>
2022-06-09 23:15:24 +0530

    Update Arrays.java

---
+++ Arrays.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Arrays{
+    public static void main(String args[]){
+        // int marks = new int[3];
+        int marks[] = {97,54,62};
+         for(int i=0 ; i<3; i++){
+            System.out.println(marks[i]);
+         }
+    }
+}
