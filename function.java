0f13108d42ed86c3d558c3d42aa5429c554ae1d6
aayushi-jain-5 <101443827+aayushi-jain-5@users.noreply.github.com>
2022-06-09 22:49:24 +0530

    Add files via upload

---
+++ function.java
@@ -0,0 +1,24 @@
+// import java.util.*;
+// public class function {
+//     public static void printName(String name){
+//         System.out.println(name);
+//         return;
+//     }
+//     public static void main(String args[]){
+//         Scanner sc= new Scanner(System.in);
+//         String name = sc.next();
+//         printName(name);
+
+//     }
+// }
+
+import java.util.*;
+public class Arrays{
+    public static void main(String args[]){
+        // int marks = new int[3];
+        int marks[] = {97,54,62};
+         for(int i=0 ; i<3; i++){
+            System.out.println(marks[i]);
+         }
+    }
+}
