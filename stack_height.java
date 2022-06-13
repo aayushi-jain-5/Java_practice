8ad354d797fbe920d1624800a7439b9a051479ff
aayushi-jain-5 <101443827+aayushi-jain-5@users.noreply.github.com>
2022-06-13 08:53:37 +0530

    Add files via upload

---
+++ stack_height.java
@@ -0,0 +1,22 @@
+import java.util.*;
+public class Recursion{
+    public static int calcPower(int x,int n){
+        if(n==0){ // base case 1
+            return 1;
+        }
+        if(x==0){ // base case 2
+            return 0;
+        }
+        if(n%2==0){ // n is even
+            return calcPower(x,n/2)* calcPower(x,n/2);
+        }
+        else{ //n is odd
+            return calcPower(x,n/2)* calcPower(x,n/2)*x;
+        }
+    }
+    public static void main(String args[]){
+        int x=2 , n=5;
+        int ans= calcPower(x,n);
+        System.out.println(ans);
+    }
+}
